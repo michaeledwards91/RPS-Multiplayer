38489ed7bb302b2b9412079258c4ad6f6fac5da5
michaeledwards91 <michael.edwards1991@gmail.com>
2016-11-20 13:10:23 -0600

    Initial files. Created HTML skeleton and linked bootstrap, jquery, and firebase.

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCVCbwLpHYOAH8cDRG882QI8-f_JClBEOw",
+authDomain: "rps-multiplayer-14d57.firebaseapp.com",
+databaseURL: "https://rps-multiplayer-14d57.firebaseio.com",
+storageBucket: "rps-multiplayer-14d57.appspot.com",
+messagingSenderId: "376014216543"
+};
+firebase.initializeApp(config);
+//
